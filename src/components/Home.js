125d88763011c6fe9da972326abd372f9836e5ff
Benji Mirschel <cliff.mirschel@gmail.com>
2019-09-02 18:26:53 -0400

    Added NavBar

---
+++ src/components/Home.js
@@ -0,0 +1,84 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <div>
+      <h2>Hello World</h2>
+      <p>
+        Cheeseburger danish fontina halloumi. Pepper jack melted cheese when the
+        cheese comes out everybody's happy cheese slices cream cheese babybel
+        port-salut stilton. Mascarpone pepper jack dolcelatte stinking bishop
+        cow babybel cheese slices caerphilly. Fromage cheddar cheese slices cow
+        monterey jack. Hard cheese gouda queso. Cheesy feet airedale stinking
+        bishop the big cheese say cheese queso cream cheese the big cheese.
+        Cheddar boursin cheese triangles camembert de normandie fromage frais
+        airedale rubber cheese cream cheese. Gouda lancashire cream cheese
+        dolcelatte goat caerphilly queso st. agur blue cheese. Macaroni cheese
+        monterey jack. Queso cauliflower cheese brie. Blue castello danish
+        fontina the big cheese rubber cheese stilton goat cheddar stilton. Goat
+        port-salut cheese strings say cheese taleggio emmental bocconcini hard
+        cheese. Feta fromage frais cow goat queso chalk and cheese cheese slices
+        pecorino. Mozzarella cheese strings red leicester. Goat airedale swiss.
+        Macaroni cheese swiss bocconcini hard cheese chalk and cheese smelly
+        cheese cauliflower cheese cheese and biscuits. Bavarian bergkase
+        macaroni cheese edam cottage cheese feta croque monsieur cheese strings
+        squirty cheese. Roquefort danish fontina cheesy feet port-salut emmental
+        blue castello pecorino taleggio. Rubber cheese hard cheese. Cheese
+        triangles jarlsberg cow. Feta cauliflower cheese caerphilly cheesecake
+        red leicester st. agur blue cheese fondue smelly cheese. Stinking bishop
+        camembert de normandie cheese strings ricotta melted cheese pepper jack
+        caerphilly croque monsieur. Roquefort stinking bishop cheesy feet.
+      </p>
+      <p>
+        Jarlsberg who moved my cheese say cheese. Cheeseburger taleggio cow
+        lancashire squirty cheese smelly cheese caerphilly danish fontina.
+        Squirty cheese monterey jack port-salut hard cheese everyone loves
+        smelly cheese red leicester roquefort. Cheesy grin ricotta cheese on
+        toast danish fontina fromage frais say cheese. Everyone loves mozzarella
+        melted cheese. Camembert de normandie cheese slices feta croque monsieur
+        pepper jack squirty cheese blue castello gouda. Red leicester smelly
+        cheese cheesecake parmesan gouda smelly cheese caerphilly rubber cheese.
+        Feta parmesan gouda edam squirty cheese melted cheese smelly cheese
+        cheese and wine. Mascarpone cheesy grin cheesy feet fondue. Hard cheese
+        pecorino pepper jack. Paneer cheese and biscuits cottage cheese cut the
+        cheese taleggio fromage frais paneer cauliflower cheese. Danish fontina
+        fromage chalk and cheese monterey jack paneer feta lancashire paneer.
+        Say cheese cheeseburger stinking bishop hard cheese brie bocconcini
+        mozzarella. Babybel fromage caerphilly. Mozzarella cow queso when the
+        cheese comes out everybody's happy monterey jack brie emmental cheesy
+        grin. Caerphilly the big cheese cheesy grin say cheese brie rubber
+        cheese camembert de normandie red leicester. Halloumi. Cheesy feet
+        cheese on toast roquefort. Boursin cheese and biscuits who moved my
+        cheese melted cheese hard cheese stinking bishop melted cheese boursin.
+        Roquefort camembert de normandie red leicester cow red leicester cheese
+        triangles feta blue castello. Everyone loves cheese and biscuits.
+      </p>
+      <p>
+        Fromage dolcelatte fromage. Babybel babybel paneer fromage halloumi
+        stilton cauliflower cheese gouda. Caerphilly say cheese cottage cheese
+        mascarpone blue castello manchego cheese slices mascarpone. Cow emmental
+        emmental when the cheese comes out everybody's happy. Everyone loves
+        melted cheese st. agur blue cheese. Port-salut cut the cheese queso
+        caerphilly fondue lancashire chalk and cheese caerphilly. The big cheese
+        cheesy feet bocconcini stinking bishop queso jarlsberg bavarian bergkase
+        parmesan. Stilton babybel. St. agur blue cheese bocconcini camembert de
+        normandie. Blue castello cheesy grin smelly cheese camembert de
+        normandie melted cheese queso airedale cream cheese. Red leicester
+        mascarpone caerphilly cheese strings cheese on toast danish fontina
+        roquefort rubber cheese. Stinking bishop emmental boursin bavarian
+        bergkase rubber cheese. Cottage cheese cream cheese bocconcini. When the
+        cheese comes out everybody's happy boursin cauliflower cheese fromage
+        frais port-salut feta cheese triangles danish fontina. Cheeseburger
+        monterey jack camembert de normandie boursin chalk and cheese cheddar
+        melted cheese feta. Stinking bishop cream cheese emmental cheese
+        triangles queso brie roquefort parmesan. Cheeseburger red leicester st.
+        agur blue cheese say cheese. Smelly cheese cut the cheese stilton.
+        Boursin boursin croque monsieur ricotta dolcelatte cheese and biscuits
+        pecorino cheeseburger. Caerphilly gouda stilton swiss fromage brie
+        cheese slices smelly cheese. Dolcelatte rubber cheese monterey jack
+        cheese slices chalk and cheese port-salut pecorino stinking bishop. When
+        the cheese comes out everybody's happy.
+      </p>
+    </div>
+  );
+};
